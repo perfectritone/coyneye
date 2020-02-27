0dd3d10c0d9c975771fbfa8273d84e64ae1a7fbe
Dave Frey <perfectritone@gmail.com>
2020-02-27 15:30:35 -0500

    Missed ,

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ use Mix.Config
 
 config :coyneye, Coyneye.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("DATABASE_URL")
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
