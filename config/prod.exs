8491a8a68def1cffd609e6fcca4e955e40d70ec1
Dave Frey <perfectritone@gmail.com>
2020-02-27 15:31:10 -0500

    Another ,

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ config :coyneye, Coyneye.Repo,
 config :coyneye, CoyneyeWeb.Endpoint,
   url: [scheme: "https", host: "coyneye.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
