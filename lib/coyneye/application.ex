ad2f375a3b18c4b465755b7eaede362c4c4b21f9
Dave Frey <perfectritone@gmail.com>
2021-03-23 01:47:06 -0400

    Always restart

--- lib/coyneye/application.ex
+++ lib/coyneye/application.ex
@@ -22,7 +22,7 @@ defmodule Coyneye.Application do
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Coyneye.Supervisor]
+    opts = [strategy: :one_for_one, name: Coyneye.Supervisor, restart: :always]
     Supervisor.start_link(children, opts)
   end
 
